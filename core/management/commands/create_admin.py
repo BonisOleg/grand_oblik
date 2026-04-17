8acf31d29fe387b84ed626d8306a27120e9a5631
Oleg Bonislavskyi <oleg@example.com>
2026-04-17 14:11:25 +0300

    super

--- core/management/commands/create_admin.py
+++ core/management/commands/create_admin.py
@@ -5,12 +5,17 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Створює суперюзера з env vars (DJANGO_SUPERUSER_USERNAME, EMAIL, PASSWORD)'
+    help = (
+        'Створює суперюзера з env vars: DJANGO_SUPERUSER_USERNAME '
+        '(або DJANGO_SUPERUSER_NAME), DJANGO_SUPERUSER_EMAIL, DJANGO_SUPERUSER_PASSWORD'
+    )
 
     def handle(self, *args, **options):
         User = get_user_model()
 
-        username = os.environ.get('DJANGO_SUPERUSER_USERNAME')
+        username = os.environ.get('DJANGO_SUPERUSER_USERNAME') or os.environ.get(
+            'DJANGO_SUPERUSER_NAME'
+        )
         email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
         password = os.environ.get('DJANGO_SUPERUSER_PASSWORD')
 
